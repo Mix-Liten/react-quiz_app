ad52eea8dfc2c25e889591e29e01a0c2ad5a1166
Mix-Liten <mixliten@gmail.com>
2021-02-12 09:35:37 +0800

    test: upload api data

---
+++ src/API.data.ts
@@ -0,0 +1,103 @@
+export const data = {
+  response_code: 0,
+  results: [
+    {
+      category: "Entertainment: Cartoon & Animations",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Who voices for Ruby in the animated series RWBY?",
+      correct_answer: "Lindsay Jones",
+      incorrect_answers: ["Tara Strong", "Jessica Nigri", "Hayden Panettiere"],
+    },
+    {
+      category: "Entertainment: Video Games",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Which of these is NOT a playable character race in the video game &quot;Starbound&quot;?",
+      correct_answer: "Fenerox",
+      incorrect_answers: ["Floran", "Novakid", "Hylotl"],
+    },
+    {
+      category: "Science & Nature",
+      type: "multiple",
+      difficulty: "medium",
+      question: "Myopia is the scientific term for which condition?",
+      correct_answer: "Shortsightedness",
+      incorrect_answers: ["Farsightedness", "Double Vision", "Clouded Vision"],
+    },
+    {
+      category: "Entertainment: Japanese Anime & Manga",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the anime Seven Deadly Sins what is the name of one of the sins?",
+      correct_answer: "Diane",
+      incorrect_answers: ["Sakura", "Ayano", "Sheska"],
+    },
+    {
+      category: "Entertainment: Video Games",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "Which character, in the game &quot;Morenatsu&quot;, has the most possible endings to their route, at a total of four different endings?",
+      correct_answer: "Shin Kuroi",
+      incorrect_answers: [
+        "Kouya Aotsuki",
+        "Soutarou Touno",
+        "Torahiko Ooshima",
+      ],
+    },
+    {
+      category: "Geography",
+      type: "multiple",
+      difficulty: "hard",
+      question: "What is the name of the Canadian national anthem?",
+      correct_answer: "O Canada",
+      incorrect_answers: [
+        "O Red Maple",
+        "Leaf-Spangled Banner",
+        "March of the Puck Drop",
+      ],
+    },
+    {
+      category: "Science & Nature",
+      type: "multiple",
+      difficulty: "hard",
+      question:
+        "What genetic disease is caused by having an extra Y chromosome (XYY)?",
+      correct_answer: "Jacob&#039;s Syndrome",
+      incorrect_answers: [
+        "Klinefelter&#039;s Syndrome",
+        "Turner&#039;s Syndrome",
+        "Down Syndrome",
+      ],
+    },
+    {
+      category: "Entertainment: Board Games",
+      type: "multiple",
+      difficulty: "medium",
+      question: "Which of the following tabletop games is the oldest?",
+      correct_answer: "Go",
+      incorrect_answers: ["Chess", "Mahjong", "Shogi"],
+    },
+    {
+      category: "Entertainment: Film",
+      type: "multiple",
+      difficulty: "medium",
+      question:
+        "Who plays &quot;Bruce Wayne&quot; in the 2008 movie &quot;The Dark Knight&quot;?",
+      correct_answer: "Christian Bale",
+      incorrect_answers: ["Michael Caine", "Ron Dean", "Heath Ledger"],
+    },
+    {
+      category: "Entertainment: Books",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+  ],
+};
